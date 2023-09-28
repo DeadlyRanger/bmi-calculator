d6267bf13a709770641c980d7a384f81bb7f8824
Mudit Kubrey <94749969+DeadlyRanger@users.noreply.github.com>
2023-09-28 12:32:10 +0530

    BMI

---
+++ script.js
@@ -0,0 +1,27 @@
+ const weight = document.querySelector("#weight");
+ const height = document.querySelector("#height");
+
+  const button = document.querySelector("#btn");
+
+  const demo = document.querySelector("#demo");
+  const result = document.querySelector("#result");
+  const output = document.querySelector("#output"); 
+
+  button.addEventListener("click",function(){
+   var a = weight.value;
+   var b = height.value;
+
+    var  c = a/b*b;
+      
+      if( a>=0 && b>=0){
+
+    
+    output.style.opacity = 1;
+    result.innerHTML = c;
+      }
+      else {
+          output.style.opacity = 1;
+        result.innerHTML = "Invalid Data !!";
+      }
+
+  })
